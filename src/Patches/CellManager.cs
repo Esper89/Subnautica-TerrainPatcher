92e05db153044ed0b444ffb31f439492058dd6a9
Esper Thomson <esperenzathomson@gmail.com>
2026-04-29 14:49:43 -0400

    Improved transpiler.

--- src/Patches/CellManager.cs
+++ src/Patches/CellManager.cs
@@ -16,60 +16,75 @@ internal static class CellManagerPatches
     [HarmonyPatch(typeof(CellManager), nameof(CellManager.RegisterCellEntity))]
     private static class CellManager_RegisterCellEntity_Patch
     {
+        // replace div and rem by blocksPerBatch with floor div and floor rem
         private static IEnumerable<CodeInstruction> Transpiler(
             IEnumerable<CodeInstruction> instructions
-        )
-        {
-            MethodInfo getBlocksPerBatch = AccessTools.PropertyGetter(
-                typeof(LargeWorldStreamer), nameof(LargeWorldStreamer.blocksPerBatch)
-            );
-
-            return new CodeMatcher(instructions)
-                .MatchStartForward([
-                    new(OpCodes.Callvirt, getBlocksPerBatch),
-                    new(OpCodes.Call, AccessTools.Method(
-                        typeof(Int3), "op_Division", [typeof(Int3), typeof(Int3)]
-                    ))
-                ])
-                .ThrowIfNotMatch(
-                    $"Could not transpile {typeof(CellManager)}." +
-                    $"{nameof(CellManager.RegisterCellEntity)}: first match failed"
-                )
-                .Advance(1)
-                .SetInstruction(new(OpCodes.Call, AccessTools.Method(
-                    typeof(CellManagerPatches), nameof(CellManagerPatches.VoxelPosToBatchId)
-                )))
-                .MatchStartForward([
-                    new(OpCodes.Callvirt, getBlocksPerBatch),
-                    new(OpCodes.Call, AccessTools.Method(
-                        typeof(Int3), "op_Modulus", [typeof(Int3), typeof(Int3)]
-                    ))
-                ])
-                .ThrowIfNotMatch(
-                    $"Could not transpile {typeof(CellManager)}." +
-                    $"{nameof(CellManager.RegisterCellEntity)}: second match failed"
-                )
-                .Advance(1)
-                .Insert([new(OpCodes.Ldloc_1)])
-                .Advance(1)
-                .SetInstruction(new(OpCodes.Call, AccessTools.Method(
-                    typeof(CellManagerPatches),
-                    nameof(CellManagerPatches.GlobalVoxelToLocalVoxelPos)
-                )))
-                .InstructionEnumeration();
-        }
+        ) => new CodeMatcher(instructions)
+            .MatchStartForward([
+                new(OpCodes.Callvirt, AccessTools.PropertyGetter(
+                    typeof(LargeWorldStreamer), nameof(LargeWorldStreamer.blocksPerBatch)
+                )),
+                new(OpCodes.Call, AccessTools.Method(
+                    typeof(Int3), "op_Division", [typeof(Int3), typeof(Int3)]
+                )),
+            ])
+            .ThrowIfNotMatch(
+                $"Could not transpile {typeof(CellManager)}." +
+                $"{nameof(CellManager.RegisterCellEntity)}: method does not contain expected code"
+            )
+            .Advance(2)
+            .Insert([
+                new(OpCodes.Pop),
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Call, AccessTools.Method(
+                    typeof(CellManagerPatches), nameof(CellManagerPatches.GetBatchId)
+                )),
+            ])
+            .Start()
+            .MatchStartForward([
+                new(OpCodes.Callvirt, AccessTools.PropertyGetter(
+                    typeof(LargeWorldStreamer), nameof(LargeWorldStreamer.blocksPerBatch)
+                )),
+                new(OpCodes.Call, AccessTools.Method(
+                    typeof(Int3), "op_Modulus", [typeof(Int3), typeof(Int3)]
+                )),
+            ])
+            .ThrowIfNotMatch(
+                $"Could not transpile {typeof(CellManager)}." +
+                $"{nameof(CellManager.RegisterCellEntity)}: method does not contain expected code"
+            )
+            .Advance(2)
+            .Insert([
+                new(OpCodes.Pop),
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Ldarg_1),
+                new(OpCodes.Call, AccessTools.Method(
+                    typeof(CellManagerPatches), nameof(CellManagerPatches.GetLocalPos)
+                )),
+            ])
+            .InstructionEnumeration();
     }
 
-    // Converts a voxel position (relative to Voxeland origin) into a batch ID. Works for positive
-    // and negative voxel positions.
-    private static Int3 VoxelPosToBatchId(Int3 block, Int3 blocksPerBatch) => new Int3(
-        Mathf.FloorToInt((float) block.x / blocksPerBatch.x),
-        Mathf.FloorToInt((float) block.y / blocksPerBatch.y),
-        Mathf.FloorToInt((float) block.z / blocksPerBatch.z)
-    );
+    private static Int3 GetBatchId(CellManager cellManager, LargeWorldEntity entity)
+    {
+        var block = cellManager.streamer.GetBlock(entity.transform.position);
+        var blocksPerBatch = cellManager.streamer.blocksPerBatch;
+        return new(
+            Utils.DivFloor(block.x, blocksPerBatch.x),
+            Utils.DivFloor(block.y, blocksPerBatch.y),
+            Utils.DivFloor(block.z, blocksPerBatch.z)
+        );
+    }
 
-    // Converts a voxel position to a local position within the given batch. Works for positive and
-    // negative voxel positions.
-    private static Int3 GlobalVoxelToLocalVoxelPos(Int3 block, Int3 batchId, Int3 blocksPerBatch)
-        => block - (batchId * blocksPerBatch);
+    private static Int3 GetLocalPos(CellManager cellManager, LargeWorldEntity entity)
+    {
+        var block = cellManager.streamer.GetBlock(entity.transform.position);
+        var blocksPerBatch = cellManager.streamer.blocksPerBatch;
+        return new(
+            Utils.RemFloor(block.x, blocksPerBatch.x),
+            Utils.RemFloor(block.y, blocksPerBatch.y),
+            Utils.RemFloor(block.z, blocksPerBatch.z)
+        );
+    }
 }
