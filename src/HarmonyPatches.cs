25abfcff91b5709d7c68a0d39c7b3b5f84646b3b
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-09-08 15:55:15 -0400

    Update HarmonyPatches.cs

--- src/HarmonyPatches.cs
+++ src/HarmonyPatches.cs
@@ -14,6 +14,12 @@ internal static class HarmonyPatches {
         new(short.MaxValue, short.MaxValue, short.MaxValue)
     );
 
+    //MOVE THIS WITH THE EXTENDED BATCHES TO WHEREVER ITS HOME IS
+    private static readonly Int3.Bounds VANILLA_OCTREE_BOUNDS = new(
+        Int3.zero,
+        new(127, 127, 127)
+    );
+
     [HarmonyPatch(
         typeof(LargeWorldStreamer),
         nameof(LargeWorldStreamer.GetCompiledOctreesCachePath)
@@ -51,23 +57,46 @@ internal static class HarmonyPatches {
         return false;
     }
 
+    /// <summary>
+    /// Subnautica's world is 4096 x 4096 x 4096m in vanilla. This leaves batches North and East
+    /// edges with 3 batches on their z/x rather than the usual 5 (This is further compounded at
+    /// their respective intersection). Extending world bounds causes the .optoctrees to be
+    /// improperly read (expecting 125 octrees when there are less), so swapping in the original
+    /// bounds when appropriate is necessary
+    /// </summary>
     [HarmonyPatch(typeof(BatchOctrees), nameof(BatchOctrees.LoadOctrees))]
-    private static class FixOctreeScrambling {
+    private static class FixVanillaPositiveBatchEdge
+    {
         private static IEnumerable<CodeInstruction> Transpiler(
-            IEnumerable<CodeInstruction> instructions
-        ) => new CodeMatcher(instructions)
-            .MatchStartForward(CodeInstruction.Call(
-                typeof(Int3.Bounds), nameof(Int3.Bounds.Contains), [typeof(Int3)]
-            ))
+            IEnumerable<CodeInstruction> instructions, ILGenerator generator
+        ) => new CodeMatcher(instructions, generator)
+            .MatchStartForward(
+                new(OpCodes.Ldfld, AccessTools.Field(typeof(BatchOctreesStreamer), 
+                        nameof(BatchOctreesStreamer.octreeBounds))),
+                new(OpCodes.Stloc_S)
+            )
             .ThrowIfNotMatch(
                 "could not transpile " +
                 $"{typeof(BatchOctrees)}.{nameof(BatchOctrees.LoadOctrees)}: method does not " +
-                $"call {typeof(Int3.Bounds)}.{nameof(Int3.Bounds.Contains)}({typeof(Int3)})"
+                $"load {nameof(BatchOctreesStreamer)}.{nameof(BatchOctreesStreamer.octreeBounds)}"
             )
-            .Advance(1)
+            .Advance(2)
+            .CreateLabel(out Label skipVanillaOctreesOverride)
             .Insert(
-                new(OpCodes.Pop),
-                new(OpCodes.Ldc_I4_1)
+                new(OpCodes.Ldarg_0),
+                new(OpCodes.Call, AccessTools.PropertyGetter(typeof(BatchOctrees),
+                    nameof(BatchOctrees.id))
+                ),
+                CodeInstruction.CallClosure(bool (Int3 batchId) => {
+                    if (TerrainPatching.patchedBatches.ContainsKey(batchId)) {
+                        return false;
+                    }
+                    if (batchId.z == 25 || batchId.x == 25) return true;
+                    return false;
+                }),
+                new(OpCodes.Brfalse_S, skipVanillaOctreesOverride),
+                CodeInstruction.LoadField(typeof(HarmonyPatches), nameof(VANILLA_OCTREE_BOUNDS)),
+                new(OpCodes.Stloc_S, 8)
             )
             .InstructionEnumeration();
     }
