242876f6393456f247eb1e7e8e3d01e916fe86be
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-09-21 17:18:18 -0400

    Problem Batch Fixer 😃

--- src/StreamedMiniWorld/FakeArrayPool.cs
+++ src/StreamedMiniWorld/FakeArrayPool.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using UWE;
+using WorldStreaming;
 
 namespace TerrainPatcher.StreamedMiniWorld;
 
@@ -8,7 +10,12 @@ namespace TerrainPatcher.StreamedMiniWorld;
 /// counter to the original purpose, allocates on demand instead of pooling. Performance seems to be
 /// in line with pooling, given meshes are not requested very often and allocations happen off the
 /// main thread.</summary>
-internal sealed class FakeArrayPool() : SplitNativeArrayPool<byte>(0, 0, 0, 0, 0, 0, 0, 0) {
+internal sealed class FakeArrayPool : SplitNativeArrayPool<byte> {
+    internal FakeArrayPool() : base(0, 0, 0, 0, 0, 0, 0, 0) {
+        SolidOctree = new(Octree.BytesPerNode, Allocator.Persistent);
+        SolidOctree.CopyFrom(new byte[]{ 1, 0, 0, 0 });
+    }
+    
     [HarmonyPatch(typeof(SplitNativeArrayPool<byte>), nameof(Get))]
     private static class FakeArrayPoolGetImpl {
         private static bool Prefix(
@@ -25,9 +32,12 @@ internal sealed class FakeArrayPool() : SplitNativeArrayPool<byte>(0, 0, 0, 0, 0
         private static bool Prefix(
             SplitNativeArrayPool<byte> __instance, ref NativeArray<byte> arr
         ) {
-            if (__instance is not FakeArrayPool) return true;
+            if (__instance is not FakeArrayPool fakeArrayPool) return true;
+            if (arr == fakeArrayPool.SolidOctree) return false;
             arr.Dispose();
             return false;
         }
     }
+
+    internal NativeArray<byte> SolidOctree { get; private set; }
 }
